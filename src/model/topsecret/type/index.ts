286c3a797dd9e9de1f68ff8850c8dc610fdb8228
alejandrocontreras <ale.contreras3110@gmail.com>
2022-03-19 23:10:30 -0300

    project documentation

--- src/model/topsecret/type/index.ts
+++ src/model/topsecret/type/index.ts
@@ -4,6 +4,12 @@
 // Poder definir los tipos durante el tiempo de diseño nos ayuda a evitar errores en tiempo de ejecución,
 // como podría ser pasar el tipo de variable incorrecto a una función.
 
+//en este caso las constantes que son declaradas son pequeñas,
+//pero en otros casos pueden ser de varias lineas de codigos,
+//por esto la importancia de separar en archivos de tipos de datos entre otros en el "MODEL"
+//esto mantendra un codigo mas limpio y facil de mantener en el tiempo
+//ya que los archivos no son de gran tamaño y son faciles de identificar.
+
 export interface coordinates {
 	x: number;
 	y: number;
